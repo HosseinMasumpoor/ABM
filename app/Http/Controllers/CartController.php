82c739e5078fbf4876f81b4ec9c1e9a0f42c22ed
Hossein <ahoseinmasumpoora@gmail.com>
2023-12-20 20:58:25 +0330

    more messages added to check cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartCheckRequest;
+use App\Models\Product;
 use App\Models\Size;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,10 @@ class CartController extends Controller
         $errors = collect();
         foreach ($cartItems as $index => $item) {
             $size = Size::find($item["sizes"]["id"]);
+            if (empty($size)) {
+                $product = Product::find($item['id']);
+                $size = $product->sizes()->where('size', $item['sizes']['size'])->first();
+            }
             $error = [];
             // چک کردن وجود سایز
             if ($size) {
@@ -25,10 +30,35 @@ class CartController extends Controller
                     $cartItems[$index]["offPercent"] = $size->product->offPercent;
                     $cartItems[$index]["off_date_from"] = $size->product->off_date_from;
                     $cartItems[$index]["off_date_to"] = $size->product->off_date_to;
-                    $message = [
-                        'text' => "قیمت محصول در فروشگاه تغییر کرده است",
-                        'type' => "price"
-                    ];
+
+                    if ($size->product->offPrice > $item["offPrice"]) {
+                        if ($item["price"] != $item["offPrice"] && $size->product->price ==  $size->product->offPrice) {
+                            $message = [
+                                'text' => "متاسفانه زمان تخفیف این محصول به اتمام رسیده‌",
+                                'type' => "discount_increase_price"
+                            ];
+                        } else {
+
+                            $message = [
+                                'text' => "قیمت این محصول افزایش پیدا کرده است‌",
+                                'type' => "increase_price"
+                            ];
+                        }
+                    } else {
+                        if ($item["price"] == $item["offPrice"] && $size->product->price !=  $size->product->offPrice) {
+                            $message = [
+                                'text' => "این محصول تخفیف خورده است",
+                                'type' => "discount_decrease_price"
+                            ];
+                        } else {
+
+                            $message = [
+                                'text' => "قیمت این محصول کاهش پیدا کرده است‌",
+                                'type' => "decrease_price"
+                            ];
+                        }
+                    }
+
                     array_push($error, $message);
                 }
 
@@ -36,13 +66,14 @@ class CartController extends Controller
                 if ($size->stock < $item["quantity"]) {
                     if ($size->stock == 0) {
                         $message = [
-                            'text' => "متأسفانه موجودی محصول به اتمام رسید است",
+                            'text' => "متاسفانه محصول با این سایز انتخاب شده دیگر موجود نیست",
                             'type' => "deleted"
                         ];
+                        $cartItems[$index]["sizes"] = null;
                     } else {
                         $cartItems[$index]["quantity"] = $size->stock;
                         $message = [
-                            'text' => "موجودی محصول کمتر از تعداد درخواستی شماست",
+                            'text' => "موجودی این محصول از تعداد انتخاب شده شما کمتر شده است",
                             'type' => "quantity"
                         ];
                     }
@@ -54,15 +85,14 @@ class CartController extends Controller
                     'text' => "سایز انتخاب شده از محصول از فروشگاه حذف شده است. دوباره به سبد خرید اضافه کنید",
                     'type' => "deleted"
                 ];
+                $cartItems[$index]["sizes"] = null;
 
                 array_push($error, $message);
             }
             $cartItems[$index]["messages"] = $error;
             $errors->push($error);
         }
-        // dd($cartItems);
+
         return response()->json($cartItems);
-        // dd($errors);
-        // dd($request->all());
     }
 }
