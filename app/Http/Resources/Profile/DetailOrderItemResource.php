70437ba9a11c901de2e91a94e1b501b6722a09ba
Hossein <ahoseinmasumpoora@gmail.com>
2024-05-26 01:30:27 +0330

    order item detail problem solved

--- app/Http/Resources/Profile/DetailOrderItemResource.php
+++ app/Http/Resources/Profile/DetailOrderItemResource.php
@@ -17,7 +17,17 @@ class DetailOrderItemResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'product' => new ProductCardResource($this->product),
+            'product' => [
+                'id' => $this->product->id ?? null,
+                'slug' => $this->product->slug ?? null,
+                'image' => $this->product->image ?? null,
+                'brand' => $this->product->brand ?? null,
+                'name' => $this->product_name,
+                'price' => $this->product_price,
+                'offPrice' => $this->product_offPrice,
+                'color' => $this->product_color,
+                'colorCode' => $this->product_colorCode,
+            ],
             'size' => $this->size,
             'price' => $this->price,
             'quantity' => $this->quantity,
